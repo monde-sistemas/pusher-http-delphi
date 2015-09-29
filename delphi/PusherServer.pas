1b471a69fa6fb32d2bd54d9bcf5d0801d99b6029
Roberto Schneiders <roberto.schneiders@monde.com.br>
2015-09-29 16:28:43 -0300

    Adds the TAsyncPusherServer to allow async requests

--- delphi/PusherServer.pas
+++ delphi/PusherServer.pas
@@ -3,7 +3,11 @@ unit PusherServer;
 interface
 
 uses
-  ComObj;
+  ComObj,
+  System.Generics.Collections,
+  System.Threading,
+  System.Classes,
+  SysUtils;
 
 type
   TOptions = set of (UseSSL);
@@ -13,8 +17,23 @@ type
     PusherServerNative: OleVariant;
   public
     constructor Create(AppID, AppKey, AppSecret: string; CustomHost: string = '';
-      Options: TOptions = [UseSSL]);
-    procedure Trigger(Channel, EventName, Message: string);
+      Options: TOptions = [UseSSL]); virtual;
+    procedure Trigger(Channel, EventName, Message: string); virtual;
+  end;
+
+  TOnErrorEvent = reference to procedure(Error: Exception);
+  TAsyncPusherServer = class(TPusherServer)
+  private
+    FOnError: TOnErrorEvent;
+    FLock: TObject;
+    function WithLock(Proc: TProc): TProc;
+    procedure WithErrorHandling(Proc: TProc);
+  public
+    property OnError: TOnErrorEvent  read FOnError write FOnError;
+    constructor Create(AppID, AppKey, AppSecret: string; CustomHost: string = '';
+      Options: TOptions = [UseSSL]); override;
+    destructor Destroy; override;
+    procedure Trigger(Channel, EventName, Message: string); override;
   end;
 
 implementation
@@ -30,6 +49,56 @@ end;
 procedure TPusherServer.Trigger(Channel, EventName, Message: string);
 begin
   PusherServerNative.Trigger(Channel, EventName, Message);
+end;
+
+{ TAsyncPusherServer }
+
+constructor TAsyncPusherServer.Create(AppID, AppKey, AppSecret,
+  CustomHost: string; Options: TOptions);
+begin
+  inherited;
+  FLock := TObject.Create;
+end;
+
+destructor TAsyncPusherServer.Destroy;
+begin
+  TMonitor.Wait(FLock, INFINITE);
+  FLock.Free;
+  inherited;
+end;
+
+procedure TAsyncPusherServer.Trigger(Channel, EventName, Message: string);
+begin
+  TTask.Run(WithLock(procedure
+    begin
+      inherited;
+    end));
+end;
+
+function TAsyncPusherServer.WithLock(Proc: TProc): TProc;
+begin
+  Result := procedure
+    begin
+      try
+        TMonitor.Enter(FLock);
+        WithErrorHandling(Proc);
+      finally
+        TMonitor.Exit(FLock);
+      end;
+    end;
+end;
+
+procedure TAsyncPusherServer.WithErrorHandling(Proc: TProc);
+begin
+  try
+    Proc;
+  except
+    on E:Exception do
+    begin
+      if Assigned(FOnError) then
+        FOnError(E);
+    end;
+  end;
 end;
 
 end.
